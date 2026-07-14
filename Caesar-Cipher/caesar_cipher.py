a28c5d1d8b00daea8540eedd4d34e9e01b3c7e07
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-07-14 17:41:24 +0400

    Update caesar_cipher.py

--- Caesar-Cipher/caesar_cipher.py
+++ Caesar-Cipher/caesar_cipher.py
@@ -3,19 +3,30 @@
 
 message = input("Enter a message: ")
 shift = int(input("Enter shift value: "))
-
 choice = input("Encrypt or Decrypt? ").lower()
-
-print("Choice:", choice)
-print("Message:", message)
-print("Shift:", shift)
 
 result = ""
 
 for char in message:
     if char.isalpha():
-        new_char = chr(ord(char) + shift)
-        result += new_char
+
+        if char.isupper():
+            start = ord('A')
+        else:
+            start = ord('a')
+
+        position = ord(char) - start
+
+        if choice == "encrypt":
+            new_position = (position + shift) % 26
+        elif choice == "decrypt":
+            new_position = (position - shift) % 26
+        else:
+            print("Invalid choice!")
+            exit()
+
+        result += chr(start + new_position)
+
     else:
         result += char
 
