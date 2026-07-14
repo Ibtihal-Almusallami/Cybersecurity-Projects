0ae48d4d7250458b1a1e47db8cc2e14546d8c4ce
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-07-14 15:10:06 +0400

    Update password_checker.py

--- Password-Strength-Checker/password_checker.py
+++ Password-Strength-Checker/password_checker.py
@@ -3,3 +3,8 @@ print("Welcome to Password Strength Checker!")
 password = input("Enter your password: ")
 
 print("Your password is:", password)
+
+if len(password) >= 8:
+    print("✅ Good! Your password has at least 8 characters.")
+else:
+    print("❌ Your password must be at least 8 characters long.")
