2362c477c208b55d820056ee9224e365f9804dc6
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-07-14 17:28:59 +0400

    Update caesar_cipher.py

--- Caesar-Cipher/caesar_cipher.py
+++ Caesar-Cipher/caesar_cipher.py
@@ -2,3 +2,9 @@
 # Cybersecurity Project #2
 message = input("Enter a message: ")
 shift = int(input("Enter shift value: "))
+
+choice = input("Encrypt or Decrypt? ").lower()
+
+print("Choice:", choice)
+print("Message:", message)
+print("Shift:", shift)
